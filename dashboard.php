ec062c48589acb3f7a4eb53e4f4d6977b7af8f84
Garudann <gk1256177@gmail.com>
2025-02-12 16:27:47 +0530

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/dasboard.css">
+</head>
+<body>
+    <!-- <div class="sidebar">
+        <h2>Dashboard</h2>
+        <ul>
+            <li onclick="window.location.href='Dashboard.html'"><i class="fas fa-home"></i> Home</li>
+            <li onclick="window.location.href='Analytics.html'"><i class="fas fa-chart-line"></i> Analytics</li>
+            <li onclick="window.location.href='profile.html'"><i class="fas fa-user"></i> User Profile</li>
+            <li onclick="window.location.href='Settings.html'"><i class="fas fa-cog"></i> Settings</li>
+            <li onclick="window.location.href='login_page.html'"><i class="fas fa-sign-out-alt"></i> Logout</li>
+        </ul>
+    </div> -->
+    <div class="main-content">
+        <div class="header">
+            <h1>Welcome, Gokul K</h1>
+            <div class="user">
+                <img src="https://randomuser.me/api/portraits/men/32.jpg" alt="User Avatar">
+                <span>Gokul</span>
+            </div>
+        </div>
+        <div class="cards">
+            <div class="card">
+                <h3>Your tasks</h3>
+                <p>Ongoing tasks: 15</p>
+            </div>
+            <div class="card">
+                <h3>Completed task</h3>
+                <p>Completed tasks: 95</p>
+            </div>
+            <div class="card">
+                <h3>Incompleted task</h3>
+                <p>Pending tasks: 24</p>
+            </div>
+        </div>
+</body>
+</html>
