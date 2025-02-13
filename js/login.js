dd0ce5b3894a25b5eb80860a6c67be10f4913728
Garudann <gk1256177@gmail.com>
2025-02-13 15:59:39 +0530

    php added into js

--- js/login.js
+++ js/login.js
@@ -14,3 +14,27 @@ document.getElementById('login_form').addEventListener('submit', function(event)
         document.getElementById('loginStatus').style.color = 'red';
     }
   });
+
+
+//   <!-- <?php
+// session_start();
+// if(isset($_POST['signin']))
+// {
+// $usenamr=$_POST['username'];
+// $password=md5($_POST['password']);
+// $sql ="SELECT username,Password FROM tblusers WHERE username=:username and Password=:password";
+// $query= $dbh -> prepare($sql);
+// $query-> bindParam(':username', $username, PDO::PARAM_STR);
+// $query-> bindParam(':password', $password, PDO::PARAM_STR);
+// $query-> execute();
+// $results=$query->fetchAll(PDO::FETCH_OBJ);
+// if($query->rowCount() > 0)
+// {
+// $_SESSION['login']=$_POST['username'];
+// echo "<script type='text/javascript'> document.location = 'package-list.php'; </script>";
+// } else{
+	
+// 	echo "<script>alert('Invalid Details');</script>";
+
+// }
+// } -->
