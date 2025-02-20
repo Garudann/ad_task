3b6c4f0b5bc294346a00acee88da53c5f086e9f8
Garudann <gk1256177@gmail.com>
2025-02-20 17:21:36 +0530

    forgot password is added

---
+++ forgot_password.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>change password</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="">
+            <h3>change your password</h3>
+            <div class="input-box">
+                <input type="text" placeholder="Mobile number" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="password" required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="confirm password" required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+            <button class="btn2">Send OTP</button>
+            <div class="input-box">
+                <input type="text" placeholder="Enter OTP" required>
+                <i class='bx bxs-chat'></i>
+            </div>
+            <button type="submit" class="btn1">submit</button>
+        </form>
+    </div>
+</body>
+</html>
