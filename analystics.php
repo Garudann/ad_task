393bf31f52543e9dffdea0e409a35dbcabbf822a
Garudann <gk1256177@gmail.com>
2025-04-09 12:14:21 +0530

    login session added

--- analystics.php
+++ analystics.php
@@ -3,13 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Analytics</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/dasboard.css">
 </head>
 <body>
     <?php
     include ('includes/sidebar.php');
+    session_start();
+
+    if (!isset($_SESSION['login'])) {
+    // User is not logged in, redirect to login page
+    echo "<script>
+        alert('Please log in first.');
+        window.location.href = 'index.php';
+    </script>";
+    exit();
+    }
     ?>
     <div class="main-content">
         <div class="header">
